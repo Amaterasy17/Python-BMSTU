d93f925f4584bcb2d4f8b104cb2a94c52f960779
Amaterasy17 <sheb70017@gmail.com>
2020-11-20 14:43:56 +0300

    have done 3 lab

--- Lab3/main.py
+++ Lab3/main.py
@@ -8,22 +8,25 @@ def main():
         {'title': 'Ковер', 'price': 2000, 'color': 'green'},
         {'title': 'Диван для отдыха', 'price': 5300, 'color': 'black'},
      ]
+    print('result field:')
     print(field(goods, 'title'))
     print(field(goods, 'title', 'price'))
 
     list = ['Abc', 'ABC', 'AbC']
     generator = gen_random(5, 15, 20)
+    print('result generator:')
     print(generator)
-
+    print('result unique with generator')
     for item in Unique(generator):
         print(item)
-
+    print('result unique with list')
     for item in Unique(list):
         print(item)
     dict = {
         'ignore_case': True,
     }
     print(' ')
+    print('result unique with list and ignore_case = true:')
     for item in Unique(list, **dict):
         print(item)
 
